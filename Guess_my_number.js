ac38f540e17eb4baddeaee327bbc95e9d414dc39
DemianDoru <dorutz_ionutz@yahoo.com>
2023-03-11 13:25:38 +0200

    more modif

---
+++ Guess_my_number.js
@@ -0,0 +1,70 @@
+'use strict';
+// // asa selectam un element si ii schimbam textul
+// document.querySelector('.message').textContent = '🏆 Correct number';
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+// // asa selectam clasa si manipulam valoarea
+// document.querySelector('.guess').value = 15;
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+document.querySelector('.check').addEventListener('click', function () {
+  let guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  // When ther is no input
+  if (!guess) {
+    document.querySelector('.message').textContent =
+      '⛔ No Number ! Write something...';
+    score--;
+    document.querySelector('.score').textContent = score;
+
+    // When player win
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent =
+      '🏆Correct Number! Congrats You Win!';
+    document.querySelector('body').style.backgroundColor = 'green';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNumber;
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+
+    // When number is too high
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too high, try less';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the Game!';
+      document.querySelector('.score').textContent = 0;
+    }
+
+    // When the number is too low
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too low, try higher';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the Game!';
+      document.querySelector('.score').textContent = 0;
+      document.querySelector('body').style.backgroundColor = 'red';
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
